3cbd8d7d2e1e884e67283d1030d33a760d0ae0db
gonzalo <pierremickael.gonzalo@gmail.com>
2020-07-20 14:18:22 +0200

    [Update]: unit test file to test the new update minibatch method

--- tests/unitTest.py
+++ tests/unitTest.py
@@ -2,17 +2,44 @@
 from network import Network
 import mnist_loader
 import numpy as np
-print("coucou")
 
+import unittest
 
-net = Network([784, 100, 10])
-net2 = Network([784, 100, 10])
+class TestNetwork(unittest.TestCase):
+    """TEST ON UPDATE_MINIBATCH METHOD"""
+    def test_update_mini_batch(self):
+        net = Network([784, 100, 10])
+        net2 = Network([784, 100, 10])
 
-training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+        training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-# sample = list(training_data)[0]
+        list_arr = list(training_data)
 
-list_arr = list(training_data)
+        eta = 0.1
+        batch = list_arr[:10]
+
+        net.update_mini_batch(batch, eta) #original method
+
+        weights1 = net.get_weights()
+        biases1 = net.get_biases()
+
+        net2.effectiv_update_mini_batch(batch, eta) #updated method
+
+        weights2 = net.get_weights()
+        biases2 = net.get_biases()
+
+        arr = [(w == w2).all() for w, w2 in zip(weights1, weights2)]
+        # print(arr)
+        self.assertEqual(False in arr, False)
+
+        arr = [(b == b2 ).all() for b, b2 in zip(biases1, biases2)]
+        
+        self.assertEqual(False in arr, False)
+        # print(arr)
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 """TEST ON BACKPROP METHOD"""
 # sample = list_arr[0]
@@ -29,24 +56,3 @@ list_arr = list(training_data)
 
 # delta_biases, delta_weights = net.effectiv_backprop(list_arr[:2])
 # print(delta_biases)
-
-"""TEST ON UPDATE_MINIBATCH METHOD"""
-
-eta = 0.1
-batch = list_arr[:10]
-
-net.update_mini_batch(batch, eta)
-
-weights1 = net.get_weights()
-biases1 = net.get_biases()
-
-net2.effectiv_update_mini_batch(batch, eta)
-
-weights2 = net.get_weights()
-biases2 = net.get_biases()
-
-arr = [(w == w2).all() for w, w2 in zip(weights1, weights2)]
-print(arr)
-
-arr = [(b == b2 ).all() for b, b2 in zip(biases1, biases2)]
-print(arr)
